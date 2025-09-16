a5572f8049621940154cbf2cf1d881cfc8424deb
Zahangir-sumit <zahangirsumitseu@gmail.com>
2025-09-16 06:17:04 -0700

    inheritence

--- Javascript/Class/cls.js
+++ Javascript/Class/cls.js
@@ -22,4 +22,24 @@ const product1 = new Product("Apple", 100.00);
 product1.displayProduct();
 
 
+// Inheritence
 
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present() {
+    return 'I have a ' + this.carname;
+  }
+}
+
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
