bdcaadb52b798b7e3b22571f8140ba808465bfb5
Zahangir-sumit <zahangirsumitseu@gmail.com>
2025-09-26 18:57:50 +0600

    interface

--- TypeScript/src/myFunctions.ts
+++ TypeScript/src/myFunctions.ts
@@ -25,3 +25,26 @@ const getHello = (s: string): string => {
 function showError(errmsg: string): never {
     throw Error(errmsg);
 } 
+
+function combine(a: string, b: string): string;
+function combine(a: number, b: number): number;
+function combine(a: any, b: any): any {
+    return a + b;
+}
+// console.log(combine(1, "2")); error
+
+
+interface Person { name: string; salary: number; }
+type Employee = { name: string; salary: number; }
+const e: Employee = { name: "John", salary: 5000 };
+const p: Person = e;
+console.log(p);
+
+
+type User = { id: number; name: string; role?: string  };
+function printUser({ id, name, role = "guest" }: User) {
+  console.log(`${id}-${name}-${role}`);
+}
+printUser({ id: 1, name: "Alice" });
+// printUser({ id: 2, name: "Bob", role: undefined }); error
+printUser({ id: 3, name: "Charlie", role: "admin" });
